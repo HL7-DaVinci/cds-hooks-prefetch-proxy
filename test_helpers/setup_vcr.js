d48749993248f8ac9088415017e12d2c87008a4a
Cole Springate-Combs <cspringatecombs@mitre.org>
2018-10-22 11:01:29 -0400

    slight yak rework, adding travis.yml

--- test_helpers/setup_vcr.js
+++ test_helpers/setup_vcr.js
@@ -2,35 +2,36 @@ const http = require("http");
 const express = require("express");
 const yakbak = require("yakbak");
 
+const YAK_PORT = 44659; // if this is changed, for some reason everything needs to be re-recorded
+
 module.exports = function(noRecord, ...servers_to_yak) {
   // use yakbak to replace the servers with proxies that record and playback http requests
-  const server_to_id = {};
-  const id_to_yak = {};
+  const server_to_idx = {};
+  const yaks = [];
   servers_to_yak.forEach(function(server, i) {
-    const id = i.toString().padStart(5, "0");
-    yak = yakbak(server, { dirname: __dirname + "/tapes", noRecord: noRecord });
-    [server_to_id[server], id_to_yak[id]] = [id, yak];
+    yaks.push(yakbak(server, { dirname: __dirname + "/tapes", noRecord: noRecord }));
+    server_to_idx[server] = i;
     console.log("yakking " + server);
   });
 
   express()
     .use(function(req, res) {
-      yak = id_to_yak[req.path.substr(1, 5)];
-      req.url = req.url.substr(6);
-      yak(req, res);
+      const id = req.path.substring(1, req.path.indexOf("/", 1));
+      req.url = req.url.substr(1 + id.length);
+      yaks[parseInt(id)](req, res);
     })
-    .listen(44659);
+    .listen(YAK_PORT);
 
   // capture requests and override, will not work if passed in url, only options
   // this has ONLY been tested to work with use of http.request by axios
   const http_request = http.request;
   http.request = function(options, callback) {
     const url = options.protocol + "//" + options.hostname + ":" + options.port;
-    if (url in server_to_id) {
-      const id = server_to_id[url];
-      [options.protocol, options.hostname, options.port] = ["http:", "localhost", 44659];
-      options.host = "localhost:44658";
-      options.path = "/" + id + options.path;
+    if (url in server_to_idx) {
+      const idx = server_to_idx[url];
+      [options.protocol, options.hostname, options.port] = ["http:", "localhost", YAK_PORT];
+      options.host = "localhost:" + YAK_PORT;
+      options.path = "/" + idx + options.path;
     }
     return http_request(options, callback);
   };
